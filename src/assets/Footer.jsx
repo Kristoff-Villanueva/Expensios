214d158a701a58d5405c0c9a4eb46f48b172fe7c
kristoffVillanueva <k.a.villanueva@accenture.com>
2022-12-14 13:47:13 +0800

    add router

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
 		<div className="footer-div">
 			<p className="footer-icons">🏠</p>
 			<p className="footer-icons">✉️</p>
-			<p className="footer-icons">📝</p>
+			<p className="footer-icons">
+				<Link to="/form">📝</Link>
+			</p>
 			<p className="footer-icons">🔔</p>
 			<p className="footer-icons">🔖</p>
 		</div>
